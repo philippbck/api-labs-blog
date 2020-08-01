313fcd4bcb29f24c70db75b7efc4aee1e55153c9
Philipp <p.buck.contact@gmail.com>
2020-08-02 00:00:33 +0200

    add fontawesome to project

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import React from "react"
+import DarkModeToggle from "./darkMode"
+import Helmet from "react-helmet"
 
 const Header = () => (
   <header>
+    <Helmet>
+    <script src="https://use.fontawesome.com/a9f203f916.js"></script>
+    </Helmet>
     <div>
-      Header
+      Header <DarkModeToggle></DarkModeToggle>
     </div>
   </header>
 )
