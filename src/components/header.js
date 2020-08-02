eb0fcc0a44009e9db2d190a5777d028563a492c7
Philipp <p.buck.contact@gmail.com>
2020-08-02 11:49:06 +0200

    fix fontawesome link

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import Helmet from "react-helmet"
 const Header = () => (
   <header>
     <Helmet>
-    <script src="https://use.fontawesome.com/a9f203f916.js"></script>
+      <script src="https://use.fontawesome.com/a9f203f916.js"></script>
     </Helmet>
     <div>
       Header <DarkModeToggle></DarkModeToggle>
