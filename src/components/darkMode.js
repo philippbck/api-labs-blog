6d5d7517764e2217ed44c7f3a7787f3414321105
Philipp <p.buck.contact@gmail.com>
2020-08-02 11:48:40 +0200

    improve dark mode slider

--- src/components/darkMode.js
+++ src/components/darkMode.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import useDarkMode from 'use-dark-mode'
-import { Toggle } from 'react-toggle-component'
 
 const DarkModeToggle = () => {
   const darkMode = useDarkMode(false);
 
   return (
     <div>
-      <span role="img" aria-label="sun">🔆</span>
-      <Toggle 
-        leftBackgroundColor="#ffffff"
-        rightBackgroundColor="#1a1919"
-        borderColor="black"
-        knobColor="#006aff"
-        name="toggle-switch"
-        checked={darkMode.value} 
-        onChange={darkMode.toggle} 
-      />
-      <span role="img" aria-label="moon">🌙</span>
+        <div class="container">
+          <label id="switch" class="switch">
+            <input type="checkbox" checked={darkMode.value} onChange={darkMode.toggle} id="slider" />
+            <span class="slider round"></span>
+          </label>
+      </div>
     </div>
   );
 };
